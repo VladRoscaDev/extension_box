247feb091ec9b785f7c9917a7ea8e6d69c1dbd16
Vlad Rosca <a1@PC253.ciurel105.stsnet.ro>
2022-01-20 09:28:10 +0200

    try again

--- test/extension_box_test.dart
+++ test/extension_box_test.dart
@@ -1,24 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:extension_box/extension_box.dart';
-export 'package:extension_box/extensions/extension_box.dart';
+export 'package:extension_box/extensions/xtension.dart';
 
-void main() {
-  const MethodChannel channel = MethodChannel('extension_box');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await ExtensionBox.platformVersion, '42');
-  });
-}
+void main() {}
